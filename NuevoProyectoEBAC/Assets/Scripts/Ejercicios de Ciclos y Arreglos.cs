49565ed32e337d12547687d5a877118a5ae9f487
Tamarindou <axelius40@gmail.com>
2025-06-25 08:19:11 -0600

    Tarea Práctica de módulo 9
    Se realizaron algunos ajustes a la tarea pero aún está incompleto el último punto, ya que no se encontró un modo óptimo de lograrlo.

--- NuevoProyectoEBAC/Assets/Scripts/Ejercicios de Ciclos y Arreglos.cs
+++ NuevoProyectoEBAC/Assets/Scripts/Ejercicios de Ciclos y Arreglos.cs
@@ -21,19 +21,11 @@ public class EjerciciosdeCiclosyArreglos: MonoBehaviour
         int valoraleatorioArreglo2 = Random.Range(limiteInferiorArreglo, limiteSuperiorArreglo);
         Debug.Log("El primer valor aleatorio generado entre 0 y 3 es " + valoraleatorioArreglo);
         Debug.Log("El segundo valor aleatorio generado entre 0 y 3 es " + valoraleatorioArreglo2);
-        
 
-        int[] MiArregloUniDimensionalUno = new int[3] { valoraleatorio, valoraleatorio2, valoraleatorio3};
-        int[] MiArregloUniDimensionalDos = new int[3] { valoraleatorio, valoraleatorio2, valoraleatorio3};
+
+        int[] MiArregloUniDimensionalUno = new int[3] { valoraleatorio, valoraleatorio2, valoraleatorio3 };
+        int[] MiArregloUniDimensionalDos = new int[3] { valoraleatorio, valoraleatorio2, valoraleatorio3 };
         int[] MiArregloUniDimensionalSuma = new int[1] { MiArregloUniDimensionalUno[valoraleatorioArreglo] + MiArregloUniDimensionalDos[valoraleatorioArreglo2] };
-
-
-        for (int i = 0; i < MiArregloUniDimensionalUno.Length; i++)
-        {
-            Debug.Log(i);
-            Debug.Log(MiArregloUniDimensionalUno[i]);
-
-        }
 
         for (int entero = 0; entero < MiArregloUniDimensionalSuma[0]; entero++)
         {
@@ -57,35 +49,34 @@ public class EjerciciosdeCiclosyArreglos: MonoBehaviour
 
         Debug.Log(saludocompleto);
 
+
         int[,] ArregloBiDimensionalUno = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
 
-        for (int x = 0; x < ArregloBiDimensionalUno.GetLength(0); x++)
-        {
-            Debug.Log("El valor x es " + x);
-
-            for (int y = 0; y < ArregloBiDimensionalUno.GetLength(1); y++)
-            {
-                for (int z = 0; z < MiArregloUniDimensionalUno.Length; z++)
-                {
-                    Debug.Log("El valor z es " + z);
-                    Debug.Log("El valor del arreglo UniDimensional según z es " + MiArregloUniDimensionalUno[z]);
-                    Debug.Log("El valor de x y y es " + ArregloBiDimensionalUno[x, y] * MiArregloUniDimensionalUno[z]);
-                }
-            }
-    }
-        //for (int a = 0; a <= MiArregloUniDimensionalUno.Length; a++)
+        //for (int a = 0; a < ArregloBiDimensionalUno.GetLength(0); a++)
         //{
-        //    Debug.Log(a);
-        //    Debug.Log(MiArregloUniDimensionalUno.Length);
-        //    Debug.Log(MiArregloUniDimensionalUno[a]);
-        //    for (int b = 0; b <= ArregloBiDimensionalUno.GetLength(1); b++)
+        //    for (int i = 0; i < MiArregloUniDimensionalUno.Length; i++)
         //    {
-        //        Debug.Log(b);
-        //        Debug.Log(ArregloBiDimensionalUno.GetLength(0));
-        //        Debug.Log(MiArregloUniDimensionalUno[b]);
+        //        Debug.Log(MiArregloUniDimensionalUno[i]);
+        //        //int Valori = MiArregloUniDimensionalUno[i];
+        //    }
+        //    for (int b = 0; b < ArregloBiDimensionalUno.GetLength(1); b++)
+        //    {
+        //        Debug.Log(ArregloBiDimensionalUno[a, b]);
+        //        //Debug.Log(ArregloBiDimensionalUno[a, b] * Valori);
         //    }
         //}
-
-
+        for (int a = 0; a < ArregloBiDimensionalUno.GetLength(0); a++)
+        {
+                for (int i = 0; i < MiArregloUniDimensionalUno.Length; i++)
+                {
+                    Debug.Log(MiArregloUniDimensionalUno[i]);
+                    //int Valori = MiArregloUniDimensionalUno[i];
+                }
+                for (int b = 0; b < ArregloBiDimensionalUno.GetLength(1); b++)
+                {
+                    Debug.Log(ArregloBiDimensionalUno[a, b]/* * Valori*/);
+                }
+                
+        }
     }
 }
